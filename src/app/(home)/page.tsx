8653fe1a9b4567599a9c7930632175f0801abc34
José Ronaldo <dev.joseronaldo@gmail.com>
2024-02-13 14:25:16 -0300

    feat: add welcome message

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,20 @@
+import { format } from "date-fns";
 import Header from "../_components/Header";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
-    <div>
+    <main>
       <Header />
-    </div>
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Ronaldo!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
+    </main>
   );
 }
